19bf4bf88152e2aed04afc2147bcdb857c045d09
Sean O'Dell <sean.odell813@gmail.com>
2020-06-10 15:47:48 -0700

    connected to data source and implement props

--- sean/articleNotes/React Book/firstReactApp/app.js
+++ sean/articleNotes/React Book/firstReactApp/app.js
@@ -1,8 +1,23 @@
 class ProductList extends React.Component {
     render() {
+        const products = Seed.products.sort((a, b) => (
+            b.votes - a.votes
+        ))
+        const productComponents = products.map(product => (
+            <Product 
+                key={`product-${product.id}`}
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}
+            />
+        ))
         return (
             <div className='ui unstackable items'>
-                <Product />
+                {productComponents}
             </div>
         );
     }
@@ -13,18 +28,28 @@ class Product extends React.Component {
         return (
             <div className='item'>
                 <div className='image'>
-                    <img src='images/products/image-aqua.png' />
+                    <img src={this.props.productImageUrl} />
                 </div>
                 <div className='middle aligned content'>
+                    <div className='header'>
+                        <a>
+                            <i className='large caret up icon' />
+                        </a>
+                        {this.props.votes}
+                    </div>
                     <div className='description'>
-                        <a>Fort Knight</a>
-                        <p>Authentic renaissance actors, delivered in just two weeks.</p>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>
+                            {this.props.description}
+                        </p>
                     </div>
                     <div className='extra'>
                         <span>Submitted by:</span>
                         <img
                             className='ui avatar image'
-                            src='images/avatars/daniel.jpg'
+                            src={this.props.submitterAvatarUrl}
                         />
                     </div>
                 </div>
