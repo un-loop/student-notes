0542e376e35fbc8f22cd084622b2a13be80732ea
ddewzy <ddewzy@gmail.com>
2020-06-11 09:49:27 -0700

    add completed app.js file

--- dalene/reactBook/app.js
+++ dalene/reactBook/app.js
@@ -1,10 +1,29 @@
 
 class ProductList extends React.Component {
-    handleProductUpVote(productId) {
-        console.log(productId + ' was upvoted.');
+  state = {
+        products: [],
+      };
+    
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
+    handleProductUpVote = (productId) => {
+        const nextProducts = this.state.products.map((product) => {
+          if (product.id === productId) {
+            return Object.assign({}, product, {
+              votes: product.votes + 1,
+            });
+          } else {
+            return product;
+          }
+        });
+        this.setState({
+          products: nextProducts,
+        });
     }
     render() {
-      const products = Seed.products.sort((a, b) => (
+      const products = this.state.products.sort((a, b) => (
           b.votes - a.votes
       ));
       const productComponents = products.map((product) => (
@@ -29,16 +48,9 @@ class ProductList extends React.Component {
   }
   
   class Product extends React.Component {
-      constructor(props) {
-          super(props);
-
-          this.handleUpVote = this.handleUpVote.bind(this);
-      }
-
-//Inside `Product`
-    handleUpVote() {
-        this.props.onVote(this.props.id);
-}
+    handleUpVote = () => (
+      this.props.onVote(this.props.id)
+    );
 
     render() {
       return (
