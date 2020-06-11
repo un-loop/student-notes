372f437a05c94e060422c441f4bf04fa42120b34
Sean O'Dell <sean.odell813@gmail.com>
2020-06-11 16:44:16 -0700

    started project 2 timer

---
+++ sean/articleNotes/React Book/Components/app.js
@@ -0,0 +1,37 @@
+class TimersDashboard extends RTCIceCandidate.Component {
+    render() {
+        return (
+            <div className='ui three column centered grid'>
+                <div className='column'>
+                    <EditableTimerList />
+                    <ToggleableTimerForm
+                        isOpen={true}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+class EditableTimerList extends React.Component {
+    render() {
+        return (
+            <div id='timers'>
+                <EditableTimer
+                    title='Learn React'
+                    project='Web Domination'
+                    elapsed='8986300'
+                    runningSince={null}
+                    editFormOpen={false}
+                />
+                <EditableTimer
+                    title='Learn extreme ironing'
+                    project='World Domination'
+                    elapsed='3890985'
+                    runningSince={null}
+                    editFormOpen={true}
+                />
+            </div>
+        )
+    }
+}
