bdde8ec077d6431c0d0a3db20e2eec82213f3e4d
Sean O'Dell <sean.odell813@gmail.com>
2020-06-22 10:02:54 -0700

    fibonacci recursion examples

---
+++ sean/lectureNotes/HopperCourse/fibonacciRecursion.js
@@ -0,0 +1,28 @@
+//iterate over huffman tree to create index key
+
+//.split() str to be compressed 
+
+//unshift() array and use return value to concat binary str onto compressed form
+
+//turn string into binary number
+
+
+//returns the nth number in the fibonacci sequence
+//called using limitedFib(n) 
+function limitedFib (n, seq = [1,1]){
+    //validates input
+    if (typeof n !== number){
+        return 'non-number input';
+    };
+    //console.log(n[1]);
+
+    //if we havn't reached the nth number in the sequence, replace the seq array with
+    //the second value and the sum of values and decrement the tracking value n
+    return (n <= 2) ? seq[1] : fib(n - 1,[seq[1], seq[0] + seq[1]]);
+};
+
+//infinitely prints fibonacci numbers in sequence to console
+function fib (seq = [1,1]){
+    console.log(seq[1]);
+    return fib([seq[1], seq[0] + seq[1]]);
+};
