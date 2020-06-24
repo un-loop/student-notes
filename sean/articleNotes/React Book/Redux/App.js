6979dc2f8c7a2f53addc497b1d4b7996f10cf69d
Sean O'Dell <sean.odell813@gmail.com>
2020-06-23 20:05:31 -0700

    integrated ground-up Redux with React App

--- sean/articleNotes/React Book/Redux/App.js
+++ sean/articleNotes/React Book/Redux/App.js
@@ -1,73 +1,162 @@
+import React from 'react'
+
 function createStore(reducer, initialState) {
-    let state = initialState;
-  
-    const getState = () => (state);
-  
-    const dispatch = (action) => {
-      state = reducer(state, action);
-    };
-  
+  let state = initialState;
+  const listeners = [];
+
+  const subscribe = (listener) => (
+    listeners.push(listener)
+  );
+
+  const getState = () => (state);
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(l => l());
+  };
+
+  return {
+    subscribe,
+    getState,
+    dispatch,
+  }
+};
+
+function reducer(state, action) {
+  if(action.type === 'ADD_MESSAGE') {
     return {
-      getState,
-      dispatch,
+      messages: state.messages.concat(action.message),
+    };
+  } else if (action.type === 'DELETE_MESSAGE'){
+    //The way the book reccomends
+    return {
+      messages: [
+        ...state.messages.slice(0, action.index),
+        ...state.messages.slice(
+          action.index + 1, state.messages.length
+        ),
+      ],
     }
+
+      //The way I would do it
+    // return {
+    //   messages: state.messages.filter(
+    //       (elem, index) => index !== action.index
+    //     )
+    // }
+  } else {
+    return state
+  }
+};
+
+const initialState = { messages: [] };
+const store = createStore(reducer, initialState);
+const listener = () => {
+  console.log('Current state:\n', store.getState());
+}
+store.subscribe(listener);
+
+
+const addMessageAction1 = {
+  type: 'ADD_MESSAGE',
+  message: 'How do you read?',
+};
+store.dispatch(addMessageAction1)
+
+const addMessageAction2 = {
+  type: 'ADD_MESSAGE',
+  message: 'I read you loud and clear, Houston'
+};
+store.dispatch(addMessageAction2)
+
+const deleteMessageAction = {
+  type: 'DELETE_MESSAGE',
+  index: 0,
+};
+store.dispatch(deleteMessageAction);
+
+class App extends React.Component {
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
+  }
+  render() {
+    const messages = store.getState().messages;
+
+    return (
+      <div className='ui segment'>
+        <MesssageView messages={messages} />
+        <MessageInput />
+      </div>
+    );
+  }
+}
+
+class MessageInput extends React.Component {
+  state = {
+    value: '',
   };
-  
-  function reducer(state, action) {
-    if(action.type === 'ADD_MESSAGE') {
-      return {
-        messages: state.messages.concat(action.message),
-      };
-    } else if (action.type === 'DELETE_MESSAGE'){
-      //The way the book reccomends
-      return {
-        messages: [
-          ...state.messages.slice(0, action.index),
-          ...state.messages.slice(
-            action.index + 1, state.messages.length
-          ),
-        ],
-      }
-  
-        //The way I would do it
-      // return {
-      //   messages: state.messages.filter(
-      //       (elem, index) => index !== action.index
-      //     )
-      // }
-    } else {
-      return state
-    }
+
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
   };
-  
-  const initialState = { messages: [] };
-  const store = createStore(reducer, initialState);
-  
-  const addMessageAction1 ={
-    type:'ADD_MESSAGE',
-    message: 'How does it look, Neil?',
+
+  handleSubmit = () => {
+    store.dispatch({
+      type: 'ADD_MESSAGE',
+      message: this.state.value,
+    });
+    this.setState({
+      value:'',
+    });
+  }
+
+  render(){
+    return (
+      <div className='ui input'>
+        <input
+          onChange={this.onChange}
+          value={this.state.value}
+          type='text'
+        />
+        <button
+          onClick={this.handleSubmit}
+          className='ui primary button'
+          type='submit'
+        >
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
+
+class MesssageView extends React.Component {
+  handleClick = (index) => {
+    store.dispatch({
+      type: 'DELETE_MESSAGE',
+      index: index,
+    });
   };
-  
-  store.dispatch(addMessageAction1);
-  const stateV1 = store.getState();
-  
-  const addMessageAction2 = {
-    type: 'ADD_MESSAGE',
-    message: 'Looking good.',
-  };
-  
-  store.dispatch(addMessageAction2);
-  const stateV2 = store.getState();
-  
-  console.log('State v1:', stateV1);
-  console.log('State v2:', stateV2);
-  
-  const deleteMessageAction = {
-    type: 'DELETE_MESSAGE',
-    index: 0,
-  };
-  
-  store.dispatch(deleteMessageAction);
-  const stateV3 = store.getState();
-  
-  console.log('State v3:', stateV3);
+
+  render() {
+    const messages = this.props.messages.map((message, index) => (
+      <div
+        className='comment'
+        key={index}
+        onClick={() => this.handleClick(index)}
+      >
+        {message}
+      </div>
+    ));
+
+    return (
+      <div className='ui comments'>
+        {messages}
+      </div>
+    );
+  }
+}
+
+export default App
