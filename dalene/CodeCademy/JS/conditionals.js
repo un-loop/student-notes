48524393d6388a2e5842ea6b1c21e866661cc4a6
ddewzy <ddewzy@gmail.com>
2020-08-11 08:54:36 -0700

    conditionals 8-11

--- dalene/CodeCademy/JS/conditionals.js
+++ dalene/CodeCademy/JS/conditionals.js
@@ -58,3 +58,54 @@ let favoritePhrase = "Love That!";
 favoritePhrase === "Love That!"
   ? console.log("I love that!")
   : console.log("I don't love that!");
+
+let isLocked = false;
+
+isLocked
+  ? console.log("You will need a key to open the door.")
+  : console.log("You will not need a key to open the door.");
+let isCorrect = true;
+isCorrect ? console.log("Correct!") : console.log("Incorrect!");
+let favoritePhrase = "Love That!";
+favoritePhrase === "Love That!"
+  ? console.log("I love that!")
+  : console.log("I don't love that!");
+
+let season = "summer";
+if (season === "spring") {
+  console.log("It's spring! The trees are budding!");
+} else if (season === "winter") {
+  console.log("It's winter! Everything is covered in snow.");
+} else if (season === "fall") {
+  console.log("It's fall! Leaves are falling!");
+} else if (season === "summer") {
+  console.log("It's sunny and warm because it's summer!");
+} else {
+  console.log("Invalid season.");
+}
+
+let athleteFinalPosition = "first place";
+switch (athleteFinalPosition) {
+  case "first place":
+    console.log("You get the gold medal!");
+    break;
+  case "second place":
+    console.log("You get the silver medal!");
+    break;
+  case "third place":
+    console.log("You get the bronze medal!");
+    break;
+  default:
+    console.log("No medal awarded.");
+    break;
+}
+
+// An if statement checks a condition and will execute a task if that condition evaluates to true.
+// if...else statements make binary decisions and execute different code blocks based on a provided condition.
+// We can add more conditions using else if statements.
+// Comparison operators, including <, >, <=, >=, ===, and !== can compare two values.
+// The logical and operator, &&, or “and”, checks if both provided expressions are truthy.
+// The logical operator ||, or “or”, checks if either provided expression is truthy.
+// The bang operator, !, switches the truthiness and falsiness of a value.
+// The ternary operator is shorthand to simplify concise if...else statements.
+// A switch statement can be used to simplify the process of writing multiple else if statements. The break keyword stops the remaining cases from being checked and executed in a switch statement.
